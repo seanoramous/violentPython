5bb94db51266de346e3d56308e37b5c78d1a6df1
seanoramous <seanoramous@gmail.com>
2017-07-09 17:55:25 +0000

    Added Threading

--- ch1ex2/unzip.py
+++ ch1ex2/unzip.py
@@ -1,20 +1,20 @@
 import zipfile
+from threading import Thread
+
 def extractFile(zFile, password):
     try:
         zFile.extractall(pwd=password)
-        return password
+        print '[+] Found password ' + password + '\n'
     except:
-        return
+        pass
 
 def main():
     zFile = zipfile.ZipFile("evil.zip")
     passFile = open('dictionary.txt')
     for line in passFile.readlines():
         password = line.strip('\n')
-        guess = extractFile(zFile, password)
-        if guess:
-            print '[+] Password = ' + password + '\n'
-            exit(0)
+        t = Thread(target=extractFile, args=(zFile, password))
+        t.start()
 
 if __name__=='__main__':
     main()
