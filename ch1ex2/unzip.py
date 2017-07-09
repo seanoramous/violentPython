03acd822aeb9e9d84d6bf61a8d90cb8a5f73f516
seanoramous <seanoramous@gmail.com>
2017-07-09 17:39:15 +0000

    Modularized code

--- ch1ex2/unzip.py
+++ ch1ex2/unzip.py
@@ -1,11 +1,20 @@
 import zipfile
-zFile = zipfile.ZipFile("evil.zip")
-passFile = open('dictionary.txt')
-for line in passFile.readlines():
-	password = line.strip('\n')
-	try:
-		zFile.extractall(pwd=password)
-		print '[+] Password = ' + password + '\n'
-		exit(0)
-	except Exception, e:
-		pass
+def extractFile(zFile, password):
+    try:
+        zFile.extractall(pwd=password)
+        return password
+    except:
+        return
+
+def main():
+    zFile = zipfile.ZipFile("evil.zip")
+    passFile = open('dictionary.txt')
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        guess = extractFile(zFile, password)
+        if guess:
+            print '[+] Password = ' + password + '\n'
+            exit(0)
+
+if __name__=='__main__':
+    main()
