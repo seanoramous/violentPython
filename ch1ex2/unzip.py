22dfba29810bb3067f845b05727858c304b4e87b
seanoramous <seanoramous@gmail.com>
2017-07-10 02:15:03 +0000

    Added parameters

--- ch1ex2/unzip.py
+++ ch1ex2/unzip.py
@@ -1,4 +1,5 @@
 import zipfile
+import optparse
 from threading import Thread
 
 def extractFile(zFile, password):
@@ -9,8 +10,21 @@ def extractFile(zFile, password):
         pass
 
 def main():
-    zFile = zipfile.ZipFile("evil.zip")
-    passFile = open('dictionary.txt')
+    parser = optparse.OptionParser("usage%prog -f <zipfile> -d <dictionary>")
+    parser.add_option('-f', dest='zname', type='string', help='specify zip file')
+    parser.add_option('-d', dest='dname', type='string', help='specify dictionary file')
+    (options, arge) = parser.parse_args()
+
+    if (options.zname == None) | (options.dname == None):
+        print parser.usage
+        exit(0)
+    else:
+        zname = options.zname
+        dname = options.dname
+        
+    zFile = zipfile.ZipFile(zname)
+    passFile = open(dname)
+    
     for line in passFile.readlines():
         password = line.strip('\n')
         t = Thread(target=extractFile, args=(zFile, password))
